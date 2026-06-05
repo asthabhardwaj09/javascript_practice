7faac68956a2ff1d65ade807a8d1b00128117359
asthabhardwaj09 <asthabhardwaj529@gmail.com>
2026-06-05 14:14:21 +0530

    mock interview

---
+++ interview3.js
@@ -0,0 +1,48 @@
+// function outer(){
+//     return function inner(){
+//         console.log("Hyy from JS");
+        
+//     }
+// }
+
+// let result= outer()
+// // console.log(result());
+// result()
+
+// function student1(a,b){
+    
+//     console.log("Hyy from Higher Order Function"+a+b)
+// }
+
+// function student2(val){
+//     return val
+
+// }
+
+
+// student2(student1(1,2))
+
+
+// let user={
+//     name:"astha",
+//     num:44
+// }
+
+// let user1 = new Object()
+
+
+function outer(){
+    let value=2
+
+    function inner(){
+        value2=3
+        console.log(value+value2)
+    }
+    inner()
+}
+
+let total=outer()
+total()
+
+
+
---
+++ interview4.js
@@ -0,0 +1,4 @@
+"use strict"
+
+name= "astha"
+console.log(name);
