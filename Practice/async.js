6e797c7cfa6a12df86e6ce87b2eac2e32c13e608
asthabhardwaj09 <asthabhardwaj529@gmail.com>
2026-06-10 23:34:13 +0530

    callback

--- Practice/async.js
+++ Practice/async.js
@@ -1,22 +1,56 @@
 // Async/Await
 
-async function GetUserData(){
-    try {
-        const response= await fetch('https://jsonplaceholder.typicode.com/posts')
-        // console.log(response);
-        
-        if(response.ok){
-            console.log("Fetching data from server");
-        }
-        else{
-            console.log("Data did not get fetched"); 
-        }
-        const data= await response.json()
-        console.log(data);
-        
-    } catch (error) {
-        console.log("error");
+async function GetUserData() {
+try {
+    const response = await fetch('https://jsonplaceholder.typicode.com/post');
+
+    if (!response.ok) {
+        throw new Error(`HTTP Error: ${response.status}`);
     }
-}
+
+    const data = await response.json();
+    console.log(data);
+
+} catch (err) {
+    console.log(err.message);
+}}
 
 GetUserData()
+
+
+async function StudentUser(){
+    setTimeout(function(){
+        console.log("Hello from Async");
+        
+    },1000)
+}
+
+StudentUser()
+
+
+console.log("A");
+
+async function test() {
+    console.log("B");
+
+    await Promise.resolve();
+
+    console.log("C");
+}
+
+test();
+
+console.log("D");
+
+
+console.log("Start");
+
+setTimeout(() => {
+    console.log("Timeout");
+}, 0);
+
+Promise.resolve().then(() => {
+    console.log("Promise");
+});
+
+console.log("End");
